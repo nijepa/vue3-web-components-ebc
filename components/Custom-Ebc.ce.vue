1ad6e273e65c4ac9bf2dde24b1a8cf3288b3e241
Nikola Pavicevic <nijepa@gmail.com>
2023-03-03 21:58:48 +0100

    email end-points calls

--- components/Custom-Ebc.ce.vue
+++ components/Custom-Ebc.ce.vue
@@ -2,24 +2,18 @@
   <section class="stage">
     <div class="container-xl stage__container">
       <h2 class="my-4" :style="{ color: primaryColor }">
-        {{ translate('title') }}
+        {{ translate("title") }}
       </h2>
-      <p class="card-text">{{ translate('text') }}</p>
+      <p class="card-text">{{ translate("text") }}</p>
       <div class="row">
         <div class="col-sm-12 col-md-6 mb-3">
           <div class="card stage__card voucher-overview">
             <div class="card-body">
               <div class="d-flex justify-content-between">
-                <h4
-                  class="overview-title"
-                  @click="togglePass"
-                >
-                  {{ translate('access-data') }}
+                <h4 class="overview-title" @click="togglePass">
+                  {{ translate("access-data") }}
                 </h4>
-                <button
-                  @click="togglePass"
-                  class="edit-btn p-0"
-                >
+                <button @click="togglePass" class="edit-btn p-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
@@ -41,7 +35,7 @@
               <form id="password-form" onsubmit="savePasswordAction(event)">
                 <div class="row my-3">
                   <div class="col-12 col-lg-4 font-weight-bold">
-                    {{ translate('user-name') }}
+                    {{ translate("user-name") }}
                   </div>
                   <div class="col-12 col-lg-8">
                     {{ user }}
@@ -49,7 +43,7 @@
                 </div>
                 <div class="row my-3">
                   <div class="col-12 col-lg-4 font-weight-bold">
-                    {{ translate('password') }}
+                    {{ translate("password") }}
                   </div>
                   <div class="col-12 col-lg-8">
                     <span id="ebc-password" class="account-data">
@@ -59,7 +53,7 @@
                       v-if="isPass"
                       id="oldPassword"
                       name="oldPassword"
-                      class="account-data form-control  mb-3"
+                      class="account-data form-control mb-3"
                       required
                       minlength="9"
                       type="password"
@@ -70,7 +64,7 @@
                       v-if="isPass"
                       id="newPassword"
                       name="newPassword"
-                      class="account-data form-control  mb-3"
+                      class="account-data form-control mb-3"
                       required
                       minlength="9"
                       type="password"
@@ -78,10 +72,10 @@
                     />
                     <p class="text-danger newPassword" hidden></p>
                     <input
-                    v-if="isPass"
+                      v-if="isPass"
                       id="newPasswordRetype"
                       name="newPasswordRetype"
-                      class="account-data form-control  mb-3"
+                      class="account-data form-control mb-3"
                       required
                       minlength="9"
                       type="password"
@@ -91,10 +85,10 @@
                     <button
                       v-if="isPass"
                       id="submit-changed-password"
-                      class="btn btn-primary account-data  mb-3 disabled"
+                      class="btn btn-primary account-data mb-3 disabled"
                       type="submit"
                     >
-                      {{ translate('save_changes') }}
+                      {{ translate("save_changes") }}
                     </button>
                   </div>
                 </div>
@@ -107,12 +101,9 @@
             <div class="card-body">
               <div class="d-flex justify-content-between">
                 <h4 class="overview-title">
-                  {{translate('delivery-address')}}
+                  {{ translate("delivery-address") }}
                 </h4>
-                <button
-                  @click="toggleEmail"
-                  class="edit-btn p-0"
-                >
+                <button @click="toggleEmail" class="edit-btn p-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
@@ -134,7 +125,7 @@
               <form id="address-form" @submit.prevent="saveAddress">
                 <div class="row my-3">
                   <div class="col-12 col-lg-4 font-weight-bold">
-                    {{ translate('email') }}
+                    {{ translate("email") }}
                   </div>
                   <div class="col-12 col-lg-8">
                     <span id="deliveryAddress">
@@ -144,36 +135,38 @@
                   <div
                     class="col-12 col-lg-4 font-weight-bold mt-4 additional-email"
                   >
-                    {{ translate('alternative_email') }}
+                    {{ translate("alternative_email") }}
                   </div>
                   <div class="col-12 col-lg-8">
-                  <span id="additionalDeliveryAddress" class="address-data">
+                    <span id="additionalDeliveryAddress" class="address-data">
                       {{ receivedData.additionalDeliveryAddress }}
                     </span>
                   </div>
-                  <div class="col-12 col-lg-8 mt-4 additional-email-data" v-if="isEmail">
-                    
+                  <div
+                    class="col-12 col-lg-8 mt-4 additional-email-data"
+                    v-if="isEmail"
+                  >
                     <input
                       id="additionalDeliveryEmailAddress"
-                      class="additional-email address-data form-control "
+                      class="additional-email address-data form-control"
                       type="email"
                       v-model="altEmail"
                       required
                     />
                     <button
                       id="submit-address"
-                      class="btn btn-primary mt-3 address-data  disabled"
+                      class="btn btn-primary mt-3 address-data disabled"
                       type="submit"
                     >
-                      {{ translate('send_to_alternative_email') }}
+                      {{ translate("send_to_alternative_email") }}
                     </button>
                     <button
                       id="delete-address"
                       onclick="deleteAddressAction(event)"
-                      class="btn btn-outline product-btn mt-3 address-data  disabled"
+                      class="btn btn-outline product-btn mt-3 address-data disabled"
                       type="submit"
                     >
-                      {{ translate('delete_alternative_email') }}
+                      {{ translate("delete_alternative_email") }}
                     </button>
                   </div>
                 </div>
@@ -187,9 +180,9 @@
 </template>
 
 <script setup>
-import { ref, computed, watch, onMounted } from 'vue';
-import { useFetch } from '../composables/useFetch';
-import { resolveUrl } from '../utils/resolveUrl';
+import { ref, computed, watch, onMounted } from "vue";
+import { useFetch } from "../composables/useFetch";
+import { resolveUrl } from "../utils/resolveUrl";
 
 // setting props
 const props = defineProps({
@@ -198,11 +191,11 @@ const props = defineProps({
   },
   primaryColor: {
     type: String,
-    default: '#fff',
+    default: "#fff",
   },
   font: {
     type: String,
-    default: '',
+    default: "",
   },
   user: {
     type: String,
@@ -216,17 +209,17 @@ const props = defineProps({
 });
 
 // return translations by keys
-const prefix = 'shop.ebc.my_account.';
+const prefix = "shop.ebc.my_account.";
 const translate = (key) => {
   return JSON.parse(props.translations)[prefix + key];
 };
 
-// fetch availabile vouchers
+// fetch user data
 const receivedData = ref([]);
-const altEmail = ref(null)
-const getData = async () => {
-  const received = await useFetch(props.emailUrl, 'POST', {
-    action: 'load_data',
+const altEmail = ref(null);
+const getUserData = async () => {
+  const received = await useFetch(props.emailUrl, "POST", {
+    action: "load_data",
   });
   received.error.length
     ? handleError(received.errorMessage)
@@ -234,32 +227,32 @@ const getData = async () => {
 };
 
 onMounted(async () => {
-  await getData();
+  await getUserData();
   console.log(receivedData.value);
 });
 
-const isPass = ref(null)
-const isEmail = ref(null)
+const isPass = ref(null);
+const isEmail = ref(null);
 const togglePass = () => {
-  isEmail.value = false
-  isPass.value = !isPass.value
-}
+  isEmail.value = false;
+  isPass.value = !isPass.value;
+};
 const toggleEmail = async () => {
-  isPass.value = false
-  isEmail.value = !isEmail.value
-  if(isEmail.value) {
-    await useFetch(props.emailUrl, 'POST', {
-    action: 'edit',
-  });
+  isPass.value = false;
+  isEmail.value = !isEmail.value;
+  if (isEmail.value) {
+    await useFetch(props.emailUrl, "POST", {
+      action: "edit",
+    });
   }
-}
+};
 
 const saveAddress = async () => {
-  await useFetch(props.emailUrl, 'POST', {
-    action: 'save',
-    additionalDeliveryEmailAddress: altEmail.value
+  await useFetch(props.emailUrl, "POST", {
+    action: "save",
+    additionalDeliveryEmailAddress: altEmail.value,
   });
-}
+};
 // // setting component state
 // const active = ref(false);
 // watch(
@@ -288,35 +281,37 @@ const saveAddress = async () => {
 </script>
 <style>
 * {
-    scroll-margin-top: 150px;
+  scroll-margin-top: 150px;
 }
 
-*, ::after, ::before {
-    box-sizing: border-box;
+*,
+::after,
+::before {
+  box-sizing: border-box;
 }
 body {
-    opacity: 1 !important;
-    font-size: 16px;
-    line-height: 1.5;
-    font-weight: 400;
-    font-family: 'Open Sans Regular', sans-serif;
-    color: #000000;
+  opacity: 1 !important;
+  font-size: 16px;
+  line-height: 1.5;
+  font-weight: 400;
+  font-family: "Open Sans Regular", sans-serif;
+  color: #000000;
 }
 .stage__container {
   position: relative;
 }
-@media (min-width: 992px){
-.stage {
+@media (min-width: 992px) {
+  .stage {
     padding: 2rem 0 3.5rem;
+  }
 }
-}
-@media (min-width: 768px){
-.stage {
+@media (min-width: 768px) {
+  .stage {
     padding: 1.5rem 0 3rem;
-}
+  }
 }
 .stage {
-    padding-bottom: 1.5rem;
+  padding-bottom: 1.5rem;
 }
 @media (min-width: 1200px) {
   .container,
@@ -369,9 +364,9 @@ body {
   border-radius: 0.25rem;
   transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
-[type='button']:not(:disabled),
-[type='reset']:not(:disabled),
-[type='submit']:not(:disabled),
+[type="button"]:not(:disabled),
+[type="reset"]:not(:disabled),
+[type="submit"]:not(:disabled),
 button:not(:disabled) {
   cursor: pointer;
 }
@@ -419,21 +414,39 @@ h2,
 .h2 {
   font-size: 1.75rem;
 }
-h4, .h4 {
-    font-size: 1.1875rem;
+h4,
+.h4 {
+  font-size: 1.1875rem;
 }
-.h4, h4 {
-    font-size: 1.5rem;
+.h4,
+h4 {
+  font-size: 1.5rem;
 }
 
-.h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
-    margin-bottom: 0.5rem;
-    font-weight: 500;
-    line-height: 1.2;
+.h1,
+.h2,
+.h3,
+.h4,
+.h5,
+.h6,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  line-height: 1.2;
 }
-h1, h2, h3, h4, h5, h6 {
-    margin-top: 0;
-    margin-bottom: 0.5rem;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin-top: 0;
+  margin-bottom: 0.5rem;
 }
 .row {
   display: -ms-flexbox;
@@ -444,12 +457,10 @@ h1, h2, h3, h4, h5, h6 {
   margin-left: -15px;
 }
 
-
 .mb-3,
 .my-3 {
   margin-bottom: 1rem !important;
 }
-
 
 .font-weight-bold {
   font-weight: 700 !important;
@@ -574,11 +585,11 @@ h6,
   }
 }
 @media (min-width: 992px) {
-.col-lg-8 {
+  .col-lg-8 {
     -ms-flex: 0 0 66.666667%;
     flex: 0 0 66.666667%;
     max-width: 66.666667%;
-}
+  }
 }
 @media (min-width: 576px) {
   .col-sm-12 {
@@ -623,8 +634,6 @@ h6,
   border: none;
 }
 
-
-
 .stage__card .card-body {
   display: flex;
   flex-direction: column;
@@ -650,69 +659,85 @@ h6,
   display: flex !important;
 }
 @media (max-width: 576px) {
-.products-list-wrapper, .voucher-overview {
+  .products-list-wrapper,
+  .voucher-overview {
     padding: 1rem 0;
     box-shadow: none;
-}
-}.btn {
-    display: inline-block;
-    font-weight: 400;
-    color: #212529;
-    text-align: center;
-    vertical-align: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background-color: transparent;
-    border: 1px solid transparent;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    border-radius: 0.25rem;
-    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  }
 }
 .btn {
-    letter-spacing: -0.0125rem;
-    padding: 0.75rem;
-    font-weight: 600;
-    border-radius: 0.5rem;
+  display: inline-block;
+  font-weight: 400;
+  color: #212529;
+  text-align: center;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+.btn {
+  letter-spacing: -0.0125rem;
+  padding: 0.75rem;
+  font-weight: 600;
+  border-radius: 0.5rem;
 }
 .btn-primary {
-    background-color: #b30000;
-    color: rgb(255, 255, 255);
-    outline: none;
-    border: 0;
+  background-color: #b30000;
+  color: rgb(255, 255, 255);
+  outline: none;
+  border: 0;
 }
-.account-data, .address-data {
-    width: 100%;
+.account-data,
+.address-data {
+  width: 100%;
 }
 
 .form-control {
-    height: 3rem;
+  height: 3rem;
 }
 .form-control:focus {
-    color: #495057;
-    background-color: #fff;
-    border-color: #80bdff;
-    outline: 0;
-    box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);
+  color: #495057;
+  background-color: #fff;
+  border-color: #80bdff;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);
 }
-.btn-primary:hover, .btn-primary:active, .btn-primary:focus, .btn-primary:not(:disabled):not(.disabled).active, .btn-primary:not(:disabled):not(.disabled):active, .btn-soft-primary:hover, .btn-soft-primary:active, .btn-soft-primary:focus, .btn-soft-primary:not(:disabled):not(.disabled).active, .btn-soft-primary:not(:disabled):not(.disabled):active, .show>.btn-primary.dropdown-toggle {
-    background-color: #660000;
-    border-color: #660000;
-    outline: none;
-    box-shadow: none;
-    color: rgb(255, 255, 255);
+.btn-primary:hover,
+.btn-primary:active,
+.btn-primary:focus,
+.btn-primary:not(:disabled):not(.disabled).active,
+.btn-primary:not(:disabled):not(.disabled):active,
+.btn-soft-primary:hover,
+.btn-soft-primary:active,
+.btn-soft-primary:focus,
+.btn-soft-primary:not(:disabled):not(.disabled).active,
+.btn-soft-primary:not(:disabled):not(.disabled):active,
+.show > .btn-primary.dropdown-toggle {
+  background-color: #660000;
+  border-color: #660000;
+  outline: none;
+  box-shadow: none;
+  color: rgb(255, 255, 255);
 }
 .product-btn {
-    border-color: #b30000;
-    color: #b30000;
+  border-color: #b30000;
+  color: #b30000;
 }
-.product-btn:hover, .product-btn:active, .product-btn:focus {
-    background-color: #b30000;
-    border-color: #b30000;
-    box-shadow: 0 0 0 0.2rem #d9d9d9;
-    color: white;
+.product-btn:hover,
+.product-btn:active,
+.product-btn:focus {
+  background-color: #b30000;
+  border-color: #b30000;
+  box-shadow: 0 0 0 0.2rem #d9d9d9;
+  color: white;
 }
 </style>
